284ed757643e1491158e220282e77399ba8e4bbf
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-10-30 21:31:54 +0200

    feat: edit category

---
+++ frontend-monolith-ts-nuxt/components/Form.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// Получаем props используя <script setup>
+// Options API либо Composition API
+const props = defineProps(["category"]);
+
+const category = props.category[0];
+
+function handleSubmit() {
+  alert(JSON.stringify(category));
+}
+</script>
+
+<template>
+  <h1>Props: {{ category }}</h1>
+
+  <form @submit.prevent="handleSubmit">
+    <input type="hidden" id="id" v-model="category.id" autocomplete="off" />
+
+    <label for="name">Name:</label>
+    <input type="text" id="name" v-model="category.name" autocomplete="off" />
+
+    <button type="submit">Submit</button>
+  </form>
+</template>
---
+++ frontend-monolith-ts-nuxt/pages/dashboard/category/edit/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute();
+
+type catDto = {
+  id: number;
+  name: string;
+};
+
+const url = "http://localhost:3001/v1/category/get/" + route.params.id;
+
+const { data: category } = await useFetch<catDto>(url);
+</script>
+
+<template>
+  <h1>Изменить категорию</h1>
+
+  <p>ID: {{ $route.params.id }}</p>
+  <p>Data: {{ category }}</p>
+
+  <Form :category="category" />
+</template>
