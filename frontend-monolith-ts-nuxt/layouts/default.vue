1f53120fb784b425fe254bb65b5547e225bf4959
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-04 22:44:57 +0200

    feat: change version && budget in menu

--- frontend-monolith-ts-nuxt/layouts/default.vue
+++ frontend-monolith-ts-nuxt/layouts/default.vue
@@ -1,16 +1,22 @@
 <template>
   <ul>
-    <li><NuxtLink to="/">Главная</NuxtLink></li>
-    <li><NuxtLink to="/version">Версия</NuxtLink></li>
-    <li><NuxtLink to="/about">About</NuxtLink></li>
+    <li>
+      <NuxtLink to="/">Главная</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/version">Версия</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/about">About</NuxtLink>
+    </li>
     <li>
       <NuxtLink to="/dashboard">Админ</NuxtLink>
       <ul>
-        <li><NuxtLink to="/dashboard/category">Категории</NuxtLink></li>
         <li>
-          <NuxtLink to="/dashboard/category/create"
-            >Категории - добавить</NuxtLink
-          >
+          <NuxtLink to="/dashboard/budget">Бюджет</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/dashboard/category">Категории</NuxtLink>
         </li>
       </ul>
     </li>
@@ -20,5 +26,5 @@
     <slot />
   </main>
 
-  <footer>&copy; 2023 &mdash; супер сайт v 0.1.3</footer>
+  <footer>&copy; 2023 &mdash; супер сайт v 0.1.5</footer>
 </template>
