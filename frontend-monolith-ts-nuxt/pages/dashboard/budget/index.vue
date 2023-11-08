661527ba02eb4468dc176e03b7bd3c3efee74a93
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-08 16:24:41 +0200

    feat: test output

--- frontend-monolith-ts-nuxt/pages/dashboard/budget/index.vue
+++ frontend-monolith-ts-nuxt/pages/dashboard/budget/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-
+const { data: budget } = await useFetch<categoryDto[]>(
+  "http://localhost:3001/v1/budget/get",
+);
 </script>
 
 <template>
   <h1>Бюджет</h1>
+
+  {{ budget }}
 
   <NuxtLink to="/dashboard/budget/create">Добавить</NuxtLink>
 </template>
