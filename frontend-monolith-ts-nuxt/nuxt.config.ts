6697e758c443783e15a9a1b2efbcec61f83c99c9
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-10 21:26:56 +0200

    feat: testing some shit

--- frontend-monolith-ts-nuxt/nuxt.config.ts
+++ frontend-monolith-ts-nuxt/nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // Enable .server.vue components
+  // and they will run ONLY on Server Side
+  experimental: {
+    componentIslands: true,
+  },
   // Enable SSR, Default TRUE
   ssr: true,
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
     "@nuxt/image",
     "@pinia/nuxt",
@@ -12,5 +17,13 @@ export default defineNuxtConfig({
   routeRules: {
     // Dashboard renders only on client-side
     "/dashboard/**": { ssr: false },
+  },
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: "123",
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: "/api",
+    },
   },
 });
