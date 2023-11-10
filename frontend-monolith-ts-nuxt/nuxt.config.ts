c3303ba191d83574c798a275e0c8e8b63ad11d49
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-10 02:18:05 +0200

    feat: pinia, scss, theme switcher

--- frontend-monolith-ts-nuxt/nuxt.config.ts
+++ frontend-monolith-ts-nuxt/nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // Enable SSR, Default TRUE
+  ssr: true,
   devtools: { enabled: true },
-  modules: ["@nuxt/image"],
-  css: ["~/assets/css/main.css"],
+  modules: [
+    "@nuxt/image",
+    "@pinia/nuxt",
+    "@nuxtjs/color-mode",
+  ],
+  css: ["~/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@use "@/assets/scss/_colors.scss" as *;',
+        },
+      },
+    },
+  },
 });
