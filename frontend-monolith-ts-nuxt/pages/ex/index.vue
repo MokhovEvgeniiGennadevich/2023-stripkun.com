b23712d12d8a604934609c618b721352bffae098
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-10 21:26:14 +0200

    feat: testing shit

---
+++ frontend-monolith-ts-nuxt/pages/ex/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>{{ counter }}</p>
+  <p>Hash: {{ hash }}</p>
+  <button @click="increment">Increment</button>
+</template>
+
+<script setup>
+import crypto from "crypto";
+
+// Server Side && User Side
+const config = useRuntimeConfig()
+const hash = config.public.apiBase
+
+if (process.server) {
+  console.log('process.server')
+}
+
+const counter = useState('counter', () => 0)
+
+function increment() {
+  counter.value++
+
+  console.log(counter)
+}
+
+// После загрузки страницы
+onBeforeMount(() => {
+  console.log('onBeforeMount')
+})
+
+onMounted(() => {
+  console.log('onMounted')
+})
+</script>
