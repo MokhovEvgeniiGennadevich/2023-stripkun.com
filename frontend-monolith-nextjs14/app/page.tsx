388ec41dbf21e2e76f539473f4ad9dbe40bf8bf7
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-11 02:19:29 +0200

    fix: clean some shit on main page

--- frontend-monolith-nextjs14/app/page.tsx
+++ frontend-monolith-nextjs14/app/page.tsx
@@ -1,19 +1,7 @@
-import crypto from "crypto";
-
 export default function HomePage() {
-  const hash = crypto
-    .createHmac("sha512", "password")
-    .update(
-      "/v1/budget/getpage0limit1" + String(Date.now()),
-      "utf-8"
-    )
-    .digest("hex");
-
   return (
     <div>
-      <h1>Home</h1>
-
-      <p>Hash: {hash}</p>
+      <h1>Финансовый стриптиз</h1>
     </div>
   );
 }
