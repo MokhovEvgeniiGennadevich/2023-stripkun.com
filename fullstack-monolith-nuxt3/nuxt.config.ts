2197d9709b2a601decde2bc21b754edb11dcc595
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-14 22:43:30 +0200

    fix: исправили конфиг, вернули всё на default значения

--- fullstack-monolith-nuxt3/nuxt.config.ts
+++ fullstack-monolith-nuxt3/nuxt.config.ts
@@ -1,29 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // Enable .server.vue components
-  // and they will run ONLY on Server Side
-  experimental: {
-    componentIslands: true,
-  },
   // Enable SSR, Default TRUE
   ssr: true,
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   modules: [
     "@nuxt/image",
     "@pinia/nuxt",
     "@nuxtjs/color-mode",
   ],
   css: ["~/assets/scss/main.scss"],
-  routeRules: {
-    // Dashboard renders only on client-side
-    "/dashboard/**": { ssr: false },
-  },
-  runtimeConfig: {
-    // The private keys which are only available server-side
-    apiSecret: "123",
-    // Keys within public are also exposed client-side
-    public: {
-      apiBase: "/api",
-    },
-  },
 });
