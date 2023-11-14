014be19291555d700574e54eb1c6b518d520af34
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-14 16:20:11 +0200

    test: output hash

--- frontend-monolith-nextjs14/app/about/page.tsx
+++ frontend-monolith-nextjs14/app/about/page.tsx
@@ -1,7 +1,23 @@
+import securityFormHash from "@/utils/securityFormHash/securityFormHash";
+
 export default function AboutPage() {
+  const securityFormHashRequest = {
+    formUrl: "/api",
+    formFileds: null,
+  };
+
+  const securityFormHashResponse = securityFormHash(
+    securityFormHashRequest
+  );
+
   return (
     <div>
       <h1>О проекте Финансовый стриптиз</h1>
+
+      <p>Версия: {securityFormHashResponse.formHash}</p>
+      <p>
+        Версия: {securityFormHashResponse.formTimeStamp}
+      </p>
 
       <p>
         Проект разрабатывался с целью учёта личных доходов и
