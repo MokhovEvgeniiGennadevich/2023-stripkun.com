97474f1dd38c9a0b0198ebedcd8333b70c064429
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-14 19:36:18 +0200

    feat: sign up page

--- frontend-monolith-nextjs14/app/auth/sign-up-with-login/page.tsx
+++ frontend-monolith-nextjs14/app/auth/sign-up-with-login/page.tsx
@@ -1,11 +1,26 @@
-import SignUpWithLoginForm from "@/components/forms/SignUpWithLogin";
+import securityFormHash, {
+  SecurityFormHashRequest,
+  SecurityFormHashResponse,
+} from "@/utils/securityFormHash/securityFormHash";
+import SignUpWithLoginForm from "./form";
 
 export default function SignUpWithLogin() {
+  // Security Form Hash
+  const securityFormHashRequest: SecurityFormHashRequest = {
+    formUrl: "/api",
+    formFileds: null,
+  };
+
+  const securityFormHashResponse: SecurityFormHashResponse =
+    securityFormHash(securityFormHashRequest);
+
   return (
     <div>
       <h1>Регистрация</h1>
 
-      <SignUpWithLoginForm />
+      <SignUpWithLoginForm
+        securityFormHash={securityFormHashResponse}
+      />
     </div>
   );
 }
