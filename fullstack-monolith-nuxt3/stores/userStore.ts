22ae3a3ebc4167bb7384969b5c6ec96456b56983
Mokhov Evgenii Gennadevich <git@mokhov-evgenii-gennadevich.com>
2023-11-14 22:42:30 +0200

    fix: remove comments, switch user

--- fullstack-monolith-nuxt3/stores/userStore.ts
+++ fullstack-monolith-nuxt3/stores/userStore.ts
@@ -10,35 +10,19 @@ export const useUserStore = defineStore(
   // $id
   "user",
   () => {
-    // if (process.server) {
-    //   // const {
-    //   //   data: quote,
-    //   //   pending,
-    //   //   error,
-    //   // } = await useFetch(
-    //   //   () => `https://dummyjson.com/quotes/${props.id}`
-    //   // );
-
-    //   const id = "gh";
-    //   const login = "user";
-    // } else {
-    //   const id = null;
-    //   const login = null;
-    // }
-
     // fake user
-    // const user: User = {
-    //   id: "fsdfdfsdfs",
-    //   login: "user",
-    //   permissions: ["dashboard.enter"],
-    // };
+    const user: User = {
+      id: "a",
+      login: "user",
+      permissions: ["dashboard.enter"],
+    };
 
     // anonymous
-    const user: User = {
-      id: null,
-      login: null,
-      permissions: [],
-    };
+    // const user: User = {
+    //   id: null,
+    //   login: null,
+    //   permissions: [],
+    // };
 
     return { user };
   }
